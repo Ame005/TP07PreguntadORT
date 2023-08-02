0ca51aca91a4be33a910c84eacc23a7aac6a52db
AMÉLIE TORRE WALSH <47757058@ort>
2023-08-02 08:14:56 -0300

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP07PreguntadORT.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
