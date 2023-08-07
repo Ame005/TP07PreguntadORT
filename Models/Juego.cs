fd9ea94f9e1fdb567955e32662339816b0c606bb
AMÉLIE TORRE WALSH <47757058@ort>
2023-08-07 08:01:39 -0300

    miniatura

---
+++ Models/Juego.cs
@@ -0,0 +1,9 @@
+class Juego {
+    string _username {get; private set;}
+    int _puntajeActual {get; private set;}
+    int _cantidadPreguntasCorrectas {get; private set;}
+    List<Pregunta> _preguntas {get; private set;}
+    List<Respuesta> _respuestas {get; private set;}
+
+    
+}
