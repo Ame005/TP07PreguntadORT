bc7c6d1411e631c6189846856d0664dc45fb18b2
AMÉLIE TORRE WALSH <47757058@ort>
2023-08-09 09:06:40 -0300

    bd terminado, juego casi

--- Models/Juego.cs
+++ Models/Juego.cs
@@ -1,29 +1,50 @@
 class Juego {
-    string _username {get; private set;}
-    int _puntajeActual {get; private set;}
-    int _cantidadPreguntasCorrectas {get; private set;}
-    List<Pregunta> _preguntas {get; private set;}
-    List<Respuesta> _respuestas {get; private set;}
+    string Username {get; private set;}
+    int PuntajeActual {get; private set;}
+    int CantidadPreguntasCorrectas {get; private set;}
+    List<Pregunta> Preguntas {get; private set;}
+    List<Respuesta> Respuestas {get; private set;}
 
-    public void InicializarJuego(){
-
+    public void InicializarJuego()
+    {
+        Username=0;
+        PuntajeActual=0;
+        CantidadPreguntasCorrectas=0;
     }
-    public List<Categoria> ObtenerCategorias(){
-
+    public List<Categoria> ObtenerCategorias()
+    {
+        return BD.ObtenerCategorias();
     }
-    public List<Dificultad> ObtenerDificultades(){
-
+    public List<Dificultad> ObtenerDificultades()
+    {
+        return BD.ObtenerDificultades();
     }
-    public void CargarPartida(string username, int dificultad, int categoria){
-
+    public void CargarPartida(string username, int dificultad, int categoria)
+    {
+        Preguntas=BD.ObtenerPreguntas(dificultad,categoria);
+        Respuestas=BD.ObtenerRespuestas(Preguntas);
     }
-    public Pregunta ObtenerProximaPregunta(){
+    public Pregunta ObtenerProximaPregunta()
+    {
+        if(Preguntas.ContainsKey())
+        {
+            Random rnd = new Random();
+            rnd.Next(1,Preguntas.Count())
+            return rnd;
+        }
+        else
+        {
+            return Preguntas.Count();
+        }
         //retorna pregunta al azar de la lista
     }
-    public Respuesta ObtenerProximasRespuestas(int idPregunta){
+    public Respuesta ObtenerProximasRespuestas(int IdPregunta)
+    {
+        return BD.ObtenerRespuestas(Preguntas(IdPregunta));//(Preguntas(Id.Pregunta)) o (IdPregunta) solo ¡preguntar binker?
         //retorna respuesta correspondiente de la lista
     }
-    public bool VerificarRespuesta(int idPregunta, int idRespuesta){
+    public bool VerificarRespuesta(int idPregunta, int idRespuesta)
+    {
 
     }
 }   
